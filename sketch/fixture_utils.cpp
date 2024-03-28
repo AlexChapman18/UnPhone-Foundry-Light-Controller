c89d09f0beab2396401b85f68f5b061a1c8f4f5d
Alex Chapman <achapman5@sheffield.ac.uk>
2024-03-28 22:04:41 +0000

    Added ability to adjust the color for DMX output

--- sketch/fixture_utils.cpp
+++ sketch/fixture_utils.cpp
@@ -7,6 +7,10 @@
 
 #include <fixture_utils.h>
 
+
+#include <artnet_utils.h>
+uint8_t ArtNetUniverse::color_universe[512] = {};
+
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ARCHITECTURE GROUP CLASS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
@@ -22,18 +26,31 @@ ArchitectureGroup::ArchitectureGroup(uint8_t *_addresses, uint8_t _num_fixtures,
 
 // Setters
 void ArchitectureGroup::setRGB(uint8_t _red, uint8_t _green, uint8_t _blue) {
-    red = _red;
-    green = _green;
-    blue = _blue;
+    ArchitectureGroup::setRed(_red);
+    ArchitectureGroup::setGreen(_green);
+    ArchitectureGroup::setBlue(_blue);
 }
 void ArchitectureGroup::setRed(uint8_t _red){
     red = _red;
+
+    for (int i = 0; i < num_fixtures; i++) {
+        uint8_t current_address = addresses[i] - 1;
+        ArtNetUniverse::color_universe[current_address] = _red;
+    }
 }
 void ArchitectureGroup::setGreen(uint8_t _green){
     green = _green;
+    for (int i = 0; i < num_fixtures; i++) {
+        uint8_t current_address = addresses[i];
+        ArtNetUniverse::color_universe[current_address] = _green;
+    }
 }
 void ArchitectureGroup::setBlue(uint8_t _blue){
     blue = _blue;
+    for (int i = 0; i < num_fixtures; i++) {
+        uint8_t current_address = addresses[i] + 1;
+        ArtNetUniverse::color_universe[current_address] = _blue;
+    }
 }
 
 // Getters
