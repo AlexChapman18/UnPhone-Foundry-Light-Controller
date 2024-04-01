0d21b35e0ea08df3392f1c16b0b7674e41b50fc7
Alex <alex.d.chapman@outlook.com>
2024-04-01 22:40:30 +0100

    Refaactored the effects to make it more readable and reduce duplicate code

--- sketch/artnet_utils.cpp
+++ sketch/artnet_utils.cpp
@@ -47,79 +47,92 @@ float ArtNetUniverse::getSpeed() { return current_speed; }
 
 float ArtNetUniverse::getIntensity() { return current_intensity; }
 
+// Effect 1, Solid
+void effect1(ArtnetWifi* artnet) {
+    for (int i = 0; i < 512; i++) {
+    artnet->setByte(i, ArtNetUniverse::current_intensity *
+                        ArtNetUniverse::color_universe[i]);
+    }
+}
+
+// Effect 2, Pulse
+void effect2(ArtnetWifi* artnet, float step) {
+    float value = abs(sin(step/(10 + (30 * (1 - ArtNetUniverse::current_speed))))) * ArtNetUniverse::current_intensity;
+    for (int i = 0; i < 512; i++) {
+        artnet->setByte(i, value * ArtNetUniverse::color_universe[i]);
+    }
+}
+
+// Effect 3, Odd even
+void effect3(ArtnetWifi* artnet, float step) {
+    float speed = 1 / (20 + 30 * (1 - (float)ArtNetUniverse::current_speed));
+    for (int i = 0; i < patch_All_Arcs_length; i++) {
+
+        float offset = i%2 * 3.14 / 2;
+        float index = step * speed + offset;
+        float intensity = abs(sin(index)) * ArtNetUniverse::current_intensity;
+
+        uint16_t red_address = patch_All_Arcs[i] - 1;
+        uint16_t green_address = patch_All_Arcs[i];
+        uint16_t blue_address = patch_All_Arcs[i] + 1;
+
+        artnet->setByte(red_address, intensity * ArtNetUniverse::color_universe[red_address]);
+        artnet->setByte(green_address, intensity * ArtNetUniverse::color_universe[green_address]);
+        artnet->setByte(blue_address, intensity * ArtNetUniverse::color_universe[blue_address]);
+    }
+}
+
+// Effect 4, Swiping
+void effect4(ArtnetWifi* artnet, float step) {
+    float speed = 1 / (20 + 30 * (1 - (float)ArtNetUniverse::current_speed));
+    for (int i = 0; i < patch_All_Arcs_length; i++) {
+
+        float offset = ((float)i / (float)patch_All_Arcs_length) * 3.14;
+        float index = step * speed + offset;
+        float intensity = abs(sin(index)) * ArtNetUniverse::current_intensity;
+
+        uint16_t red_address = patch_All_Arcs[i] - 1;
+        uint16_t green_address = patch_All_Arcs[i];
+        uint16_t blue_address = patch_All_Arcs[i] + 1;
+
+        artnet->setByte(red_address, intensity * ArtNetUniverse::color_universe[red_address]);
+        artnet->setByte(green_address, intensity * ArtNetUniverse::color_universe[green_address]);
+        artnet->setByte(blue_address, intensity * ArtNetUniverse::color_universe[blue_address]);
+    }
+}
+
+// Thread for sending the universe
 void keepSendingUniverse(void *params) {
+    uint8_t last_effect = 0;
+    float step = 0;
+    float value;
+    float speed;
+
     while (true) {
-        float step = 0;
-
-        // Effect 1
-        while (ArtNetUniverse::current_effect == 0){
-            for (int i = 0; i < 512; i++) {
-                artnet.setByte(i, ArtNetUniverse::current_intensity *
-                                    ArtNetUniverse::color_universe[i]);
-            }
-            artnet.write();
-            vTaskDelay(60 / portTICK_RATE_MS);
-        }
-
-        // Effect 2
-        while (ArtNetUniverse::current_effect == 1){
-
-            float value = abs(sin(step/(100 + (200 * (1 - ArtNetUniverse::current_speed))))) * ArtNetUniverse::current_intensity;
-            for (int i = 0; i < 512; i++) {
-                artnet.setByte(i, value * ArtNetUniverse::color_universe[i]);
-            }
-            step++;
-            artnet.write();
-            vTaskDelay(60 / portTICK_RATE_MS); // Wait 60 MS and send again
-        }
-
-
-        // Effect 3
-        while (ArtNetUniverse::current_effect == 2){
-
-            float speed = 1 / (25 + 200 * (1 - (float)ArtNetUniverse::current_speed));
-            for (int i = 0; i < patch_All_Arcs_length; i++) {
-
-                float offset = i%2 * 3.14 / 2;
-                float index = step * speed + offset;
-                float intensity = abs(sin(index)) * ArtNetUniverse::current_intensity;
-
-                uint16_t red_address = patch_All_Arcs[i] - 1;
-                uint16_t green_address = patch_All_Arcs[i];
-                uint16_t blue_address = patch_All_Arcs[i] + 1;
-
-                artnet.setByte(red_address, intensity * ArtNetUniverse::color_universe[red_address]);
-                artnet.setByte(green_address, intensity * ArtNetUniverse::color_universe[green_address]);
-                artnet.setByte(blue_address, intensity * ArtNetUniverse::color_universe[blue_address]);
-            }
-            step++;
-            artnet.write();
-            vTaskDelay(60 / portTICK_RATE_MS); // Wait 60 MS and send again
-        }
-
         
-        // Effect 4
-        while (ArtNetUniverse::current_effect == 3){
-
-            float speed = 1 / (25 + 200 * (1 - (float)ArtNetUniverse::current_speed));
-            for (int i = 0; i < patch_All_Arcs_length; i++) {
-
-                float offset = ((float)i / (float)patch_All_Arcs_length) * 3.14;
-                float index = step * speed + offset;
-                float intensity = abs(sin(index)) * ArtNetUniverse::current_intensity;
-
-                uint16_t red_address = patch_All_Arcs[i] - 1;
-                uint16_t green_address = patch_All_Arcs[i];
-                uint16_t blue_address = patch_All_Arcs[i] + 1;
-
-                artnet.setByte(red_address, intensity * ArtNetUniverse::color_universe[red_address]);
-                artnet.setByte(green_address, intensity * ArtNetUniverse::color_universe[green_address]);
-                artnet.setByte(blue_address, intensity * ArtNetUniverse::color_universe[blue_address]);
-            }
+        // Everytime you change effect, reset the step counter
+        if (last_effect != ArtNetUniverse::current_effect) {
+            step = 0;
+            last_effect = ArtNetUniverse::current_effect;
+        } else {
             step++;
-            artnet.write();
-            vTaskDelay(60 / portTICK_RATE_MS); // Wait 60 MS and send again
         }
-        vTaskDelay(100 / portTICK_RATE_MS);
+
+        switch (ArtNetUniverse::current_effect) {
+            case 0:
+                effect1(&artnet);
+                break;
+            case 1:
+                effect2(&artnet, step);
+                break;
+            case 2:
+                effect3(&artnet, step);
+                break; 
+            case 3:
+                effect4(&artnet, step);
+                break;
+        }
+        artnet.write();
+        vTaskDelay(60 / portTICK_RATE_MS); // Wait 60 MS and send again
     }
 }
