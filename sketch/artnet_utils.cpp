356ec99d0cba974568c17f33fe9d5da6d4f525a2
Alex <alex.d.chapman@outlook.com>
2024-04-02 19:09:03 +0100

    Re-added the universe to artnet_utils

--- sketch/artnet_utils.cpp
+++ sketch/artnet_utils.cpp
@@ -29,7 +29,7 @@ void ArtNetUniverse::setup() {
     current_intensity = 1;
 
     artnet.begin(TARGET_IP);
-    artnet.setUniverse(UNIVERSE_INDEX);
+    artnet.setUniverse(5);
     artnet.setLength(512);
 }
 
