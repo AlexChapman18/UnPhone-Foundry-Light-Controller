f2b3e87ec13bb80ae4627d50b5e8b53ec98d1b88
Kush Bharakhada <97338412+KushBharakhada@users.noreply.github.com>
2024-05-02 17:12:01 +0100

    Add private.h and update image.

---
+++ sketch/private.h
@@ -0,0 +1,15 @@
+/**
+ * Contains private information that is untracked by git
+ * 
+ * Author: Kush Bharakhada and Alex Chapman
+ * private.h
+*/
+
+#ifndef PRIVATE_H
+#define PRIVATE_H
+
+#define WIFI_SSID "SSID"
+#define WIFI_PASSWORD "PASSWORD"
+const char TARGET_IP[] = "0.0.0.0"; // IP of the art-net node
+
+#endif
